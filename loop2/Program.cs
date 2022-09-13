85e58a766b1a63f1a19d895e58e23e45e80cdf53
rupesh palve <rupesh.palve48@outlook.com>
2022-09-13 12:16:59 -0400

    new file on loop

--- loop2/Program.cs
+++ loop2/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace loop2
 {
@@ -10,22 +6,222 @@ namespace loop2
     {
         static void Main(string[] args)
         {
+            //prime or not..
+
+            //int k=0;
+            //Console.WriteLine("enter the number");
+            //int i = Convert.ToInt32(Console.ReadLine());
+
+            //for(int j=1; j<=i; j++)
+            //{
+            //    if(i%j==0)
+            //    {
+            //        k++;
+            //    }
+            //}
+            //if(k==2)
+            //{
+            //    Console.WriteLine("is prime number");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("is not prime number");
+            //}
+
+            //2..prime numbers from 1to 100
+
+
+            //for (int i = 2; i <= 100; i++)
+            //{
+            //    int k = 0;
+            //    for (int j = 2; j <= 100; j++)
+            //    {
+            //        if (i != j && i % j == 0)
+            //        {
+            //            k++;
+            //            break;
+            //        }
+            //    }
+            //    if (k == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+
+            //3... armstrong number is or not
+            //int n1, arm = 0, num;
+            //Console.WriteLine("enter the number");
+            //n1= Convert.ToInt32(Console.ReadLine());
+
+            //int a = n1;
+            //while(n1>0)
+            //{
+            //    num = n1 % 10;
+            //    arm = (num * num * num) + arm;
+            //    n1 = n1 / 10;
+            //}
+            //if (a == arm)
+            //{
+            //    Console.WriteLine($"{arm} is armstrong number");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{arm} is not armstrong number");
+            //}
+
+
+            //4...palindrome number
+            //int n, sum = 0, num;
+            //Console.WriteLine("enter the number");
+            //n= Convert.ToInt32(Console.ReadLine());
+
+            //int a = n;
+            //while(n>0)
+            //{
+            //    num = n % 10;
+            //    sum = (sum * 10) + num;
+            //    n = n / 10;
+            //}
+            //if(a==sum)
+            //{
+            //    Console.WriteLine($"{a} is palinodrome number"); 
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{a} is not palinodrome number");
+            //}
+
+
+            //5.. progranm on rectangle 
+            //double length, breadth, peri;
+            //Console.WriteLine("enter length and breadth");
+            //length=Convert.ToDouble(Console.ReadLine());
+            //breadth=Convert.ToDouble(Console.ReadLine());
+
+            //peri= 2*(length+breadth);
+            //Console.WriteLine("perimeter of rectangle is " + peri);
+
+
+            //6... redius of circle
+
+            //double radius, area;
+            //Console.WriteLine("enter the radius of circle");
+            //radius= Convert.ToDouble(Console.ReadLine());
+
+            //area = 3.14 * radius * radius;
+            //Console.WriteLine("area of circle is "+area);
+
+
+            //7...multiplication
+
+            //int p, q, sum = 0;
+            //Console.WriteLine("enter the two numbers");
+            //p=Convert.ToInt32(Console.ReadLine());
+            //q=Convert.ToInt32(Console.ReadLine());
+
+            //for(int i=1; i<=p; i++)
+            //{
+            //    sum = sum + q;
+            //}
+            //Console.WriteLine("the multiplication is " + p + ", " + q + " is " + sum);
+            //homework:
+
+            //1...
+
+            Console.WriteLine("enter a number");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j.ToString());
+                }
+                Console.WriteLine();
+            }
+
+
+            // 2...
+            //Console.WriteLine("enter a number");
+            //int n = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 5; j >= i; j--)
+            //    {
+            //        Console.Write(j.ToString());
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+
+
+            //4....
+            //Console.WriteLine("enter a number");
+            //int n = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = n; i >= 0; i--)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write(j.ToString());
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            //3...
+            //Console.WriteLine("enter a character");
+            //int n = Convert.ToInt32(Console.ReadLine());
+
+            //int n = 5;
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write((char)(j + 64));
+
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //Console.WriteLine();
+
+
+            //5..,
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int k = 5; k >= 1; k--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+
 
             //7..print the fibonacci series number of digit in o/p 7
             //0 1 1 2 3 5 8
-            int n, n1 = 0, n2 = 1, n3;
-            Console.WriteLine("enter the upper limit");
-            n=Convert.ToInt32(Console.ReadLine());    
-            Console.WriteLine($" {n1} {n2}");
+            //int n, n1 = 0, n2 = 1, n3;
+            //Console.WriteLine("enter the upper limit");
+            //n=Convert.ToInt32(Console.ReadLine());    
+            //Console.WriteLine($" {n1} {n2}");
 
-            for (int i = 2; i < n; i++)
-            {
-                n3 = n1 + n2;
-                Console.WriteLine($"{n3}");
-                n1 = n2;
-                n2 = n3;
-            }
-            
+            //for (int i = 2; i < n; i++)
+            //{
+            //    n3 = n1 + n2;
+            //    Console.WriteLine($"{n3}");
+            //    n1 = n2;
+            //    n2 = n3;
+            //}
+
 
             // 6..acceept the numbers from users find the factorial
 
@@ -49,14 +245,14 @@ namespace loop2
             //}
 
 
-          //Console.WriteLine("enter the number");
+            //Console.WriteLine("enter the number");
             //int num=Convert.ToInt32(Console.ReadLine());
             //for (int i = 1; i <=10; i++)
             //{
             //    Console.WriteLine($"{num} * {1}={num*i}");
             //}
 
-                    
+
             //for (int i =100; i>=50;i--)
             //{
             //    if(i%2!=0)
